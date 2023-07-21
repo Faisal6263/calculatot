51dfe210f813e83876973a99eb2ea5ea93f81379
Techcronus\Prabhuj <developerTBS1@gmail.com>
2023-07-21 17:21:54 +0530

    this is getting commited

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    var phrases = ["MS D365 Consultant", "A coder", "A Designer", "An Editor", "A Creative Thinker"];
+    var index = 0;
+    var charIndex = 0;
+    var speed = 60; // Speed of typing in milliseconds
+    var delay = 2000; // Delay between phrases in milliseconds
+  
+    function type() {
+      if (charIndex < phrases[index].length) {
+        $("#name").append(phrases[index].charAt(charIndex));
+        charIndex++;
+        setTimeout(type, speed);
+      } else {
+        setTimeout(function() {
+          $("#name").text("");
+          charIndex = 0;
+          index = (index + 1) % phrases.length;
+          type();
+        }, delay);
+      }
+    }
+    type();
+  });
+  
