0406f3ee476af9d6dc184ada6ba760d23c4fb6b8
Techcronus\Prabhuj <developerTBS1@gmail.com>
2023-07-24 10:34:46 +0530

    changes

--- script.js
+++ script.js
@@ -4,21 +4,20 @@ $(document).ready(function() {
     var charIndex = 0;
     var speed = 60; // Speed of typing in milliseconds
     var delay = 2000; // Delay between phrases in milliseconds
-  
+
     function type() {
-      if (charIndex < phrases[index].length) {
-        $("#name").append(phrases[index].charAt(charIndex));
-        charIndex++;
-        setTimeout(type, speed);
-      } else {
-        setTimeout(function() {
-          $("#name").text("");
-          charIndex = 0;
-          index = (index + 1) % phrases.length;
-          type();
-        }, delay);
-      }
+        if (charIndex < phrases[index].length) {
+            $("#name").append(phrases[index].charAt(charIndex));
+            charIndex++;
+            setTimeout(type, speed);
+        } else {
+            setTimeout(function() {
+                $("#name").text("");
+                charIndex = 0;
+                index = (index + 1) % phrases.length;
+                type();
+            }, delay);
+        }
     }
     type();
-  });
-  
+});
